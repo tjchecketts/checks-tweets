42d714ad2041dd6ffa69435f8a124e1b898167b5
“TJ <tylerchecketts@gmail.com>
2018-03-16 10:58:37 -0600

    add semanticui, axios, and post tweets

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Grid, Input, Header, Button } from 'semantic-ui-react';
+import axios from 'axios';
+import Tweets from './Tweets';
 
-class App extends Component {
+class App extends React.Component {
+  state = { tweets: [], visible: [], tweet: '' }
+
+  componentDidMount() {
+    axios.get('/api/tweets')
+      .then( res => this.setState({ tweets: res.data }) )
+  }
+
+  updateTweet = (e) => {
+    this.setState({ tweet: e.target.value })
+  }
+
+  postTweet = () => {
+    let { tweet, visible } = this.state;
+    if (tweet) {
+      axios.post('/api/tweet', { tweet })
+        .then( res => this.setState({ tweet: '', visible: [...visible, res.data] }) )
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column mobile={16} tablet={16} computer={4}>
+            <Header as="h3" textAlign="center">Tweet Something</Header>
+            <Input size="large" onChange={this.updateTweet} value={this.state.tweet} />
+            <Button onClick={this.postTweet}>Tweet!</Button>
+          </Grid.Column>
+          <Grid.Column mobile={16} tablet={16} computer={10}>
+            <Tweets tweets={ this.state.tweets }/>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
